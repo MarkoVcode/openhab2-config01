a3f62e522ce5b3fbd5885a339e5ed788b978e06f
MarkoV <marek.kaszewski@gmail.com>
2020-04-20 22:07:30 +0100

    adding translite thing

--- openhab2/deploy.sh
+++ openhab2/deploy.sh
@@ -7,12 +7,14 @@ cp etc/sitemaps/weyni.sitemap /etc/openhab2/sitemaps
 cp etc/items/http.items /etc/openhab2/items
 cp etc/items/unifi.items /etc/openhab2/items
 cp etc/items/sonoff.items /etc/openhab2/items
+cp etc/items/mqtt.items /etc/openhab2/items
 
 cp etc/services/http.cfg /etc/openhab2/services
 cp etc/services/addons.cfg /etc/openhab2/services
 
 cp etc/things/unifi.things /etc/openhab2/things
 cp etc/things/sonoff.things /etc/openhab2/things
+cp etc/things/mqtt.things /etc/openhab2/things
 
 cp etc/transform/centralLine.js /etc/openhab2/transform
 cp etc/transform/humidity.js /etc/openhab2/transform
